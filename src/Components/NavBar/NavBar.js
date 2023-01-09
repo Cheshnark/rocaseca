f038db9fa8a156a2db92f111bcc62c2a613abeaf
csnark <csnark.dev@gmail.com>
2023-01-09 17:51:15 +0100

    NavBar reforged

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,96 @@
+import './NavBar.css';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/useLogout';
+import { useUsersContext } from '../../hooks/useUsersContext';
+
+import DrawerComp from '../DrawerComp/DrawerComp';
+import logo from '../../logo2.png';
+
+const NavBar = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const [shown, setShown] = useState(false); 
+    const [hidden,setHidden] = useState("visible")
+    const { logout } = useLogout();
+    const { user } = useUsersContext()
+
+    useEffect( () => {
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => window.removeEventListener('scroll', handleScroll)
+    })
+
+    const handleClick = () => {
+        setShown(!shown);
+        
+        if(hidden === "visible"){
+          document.body.style.overflow = "hidden";
+          setHidden("hidden");
+        }else{
+          document.body.style.overflow = "visible";
+          setHidden("visible");
+        };    
+      };
+
+    const logoutClick = () => {
+      logout();
+    }
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+  
+      if(currentScrollPos > prevScrollPos){
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+  
+      setPrevScrollPos(currentScrollPos);
+    }
+
+    return(
+        <nav style={{position:visible && 'top-0', visibility: !visible && 'hidden'}}
+          className={`${visible ? 'smooth' : 'invisible'}`} >
+          <div className="nav-container">
+            <div className="img-container">
+              <Link to='/'><img src={logo} alt="rocaseca-logo" /></Link> 
+            </div>
+            
+            <i className="mobile-navigation fa-solid fa-bars" onClick={handleClick} style={{visibility: shown && 'hidden'}} />  
+            <div className='navigation'>
+               {!user && (
+                <ul className="nav-unlogged">
+                  <Link to='/search'><li>Buscador</li></Link>
+                  <Link to='/register'><li>Registrate</li></Link>
+                  <Link to='/login'><li>Login</li></Link>
+                </ul>
+               )}
+              {user && (
+              <ul className="nav-logged">
+                <Link to='/search'><li>Buscador</li></Link>
+                <Link to={'/favorites'}>
+                  <li>Favoritos</li>
+                </Link>
+                <Link to={'/profile'}>
+                  <li>Perfil</li>
+                </Link>
+                <li>
+                  <button className='navbar-logout' onClick={logoutClick}>Log out</button>  
+                </li>
+              </ul> 
+              )}        
+            </div>
+          {shown && (
+            <div onClick={handleClick}>
+              <div className="div-drawercomp">
+                <DrawerComp />
+              </div>
+            </div>
+          )}
+          </div>
+        </nav>
+    )
+};
+
+export default NavBar;
